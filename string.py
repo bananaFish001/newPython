48e5da829ec8241e04ff2157cc11b3e768ee136e
bananaFish001 <raj123.rs308@gmail.com>
2024-03-14 22:01:53 +0530

    some more python

---
+++ string.py
@@ -0,0 +1,25 @@
+print("A Computer science portal for geeks")
+
+# Creating a string with single quotes:
+string1 = "welcome to the nhk"
+print("string with the use of single quotes:")
+print(string1)
+
+# Creating string with triple quotes
+string2 = '''i want to be backend dev'''
+print("\nthis string was written in the triple quotes")
+print(string2)
+
+string3 = '''
+Geek 
+for 
+seeks
+'''
+
+print(string3)
+
+# Reversing the string:
+var = "we are the champion"
+print(var[::-1])
+
+
